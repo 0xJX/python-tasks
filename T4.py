9334a3dc2a3fc62cac17f1c896001d3988cbae7b
JX <52764999+0xJX@users.noreply.github.com>
2021-12-20 18:26:28 +0200

    Updated T4 with correct url.

--- T4.py
+++ T4.py
@@ -1,20 +1,32 @@
 import urllib.request, json
 
-def GetRegisteredCompanies(city):
-    url = f"https://avoindata.prh.fi/bis_v1.fi.json/bis/v1?totalResults=false&maxResults=10&resultsFrom=0&registeredOffice={city}&companyForm=OY&companyRegistrationFrom=1978-03-14"
-    request = urllib.request.Request(url, headers={'content-type': 'application/json', 'Accept': 'text/html'})
+header = {'content-type': 'application/json', 'Accept': 'text/html'}
+
+def GetRegisteredCompanies(city, result_limit):
+    url = f"https://avoindata.prh.fi/bis/v1?totalResults=true&maxResults={result_limit}&resultsFrom=0&registeredOffice={city}&companyForm=OY&companyRegistrationFrom=1978-03-14"
+    request = urllib.request.Request(url, headers = header)
 
     try:
         with urllib.request.urlopen(request) as response:
             result = json.loads(response.read())
-            print(result) # Printtaa koko vastauksen, ilman käsittelyä toistaiseksi kunnes saan validin vastauksen..
+            totalResults = result["totalResults"] # Näyttää kätevästi kokonaismäärän, result_limit ei vaikuta tähän arvoon.
+            print(f"{city} Kokonaismäärä: {totalResults}")
+
+            endedCompanies = 0
+            for loopedResult in result["results"]:
+                if loopedResult.get("detailsUri"):
+                    request2 = urllib.request.Request(loopedResult["detailsUri"], headers = header)
+                    with urllib.request.urlopen(request2) as response2:
+                        result2 = json.loads(response2.read())
+                        for idx, r in enumerate(result2["results"]):
+                            names = r["names"][idx].get("endDate")
+                            if "None" not in str(names):
+                                endedCompanies += 1
+
+            print(f"Lopettanut: {endedCompanies}") # Näyttää result_limitin sisällä olevat, ei siis kokonaismäärää databasessa.
     except Exception as e:
-        print(e) # Virhe, tässä tapauksessa yleisin 404 :(
+        print(e) 
 
-GetRegisteredCompanies("Ylitornio")
-GetRegisteredCompanies("Merikarvia")
-GetRegisteredCompanies("Parikkala")
-
-# Valitettavasti tätä en saanut toimimaan, 404 tuli joka kerta vastauksena vaikka kokeilin heidän omaa palvelinta, postman-ohjelmaa ja muita tapoja.
-# URL https://avoindata.prh.fi/bis_v1.fi.json näyttää toimivan, mutta https://avoindata.prh.fi/bis_v1.fi.json/bis/v1? + args palauttaa aina 404.
-# Olisin jatkanut resultin käsittelyä jos olisin saanut edes yhden vastauksen api:lta ja nähnyt mitä se puskisi ulos.
+GetRegisteredCompanies(city="Ylitornio", result_limit=50)
+GetRegisteredCompanies(city="Merikarvia", result_limit=50)
+GetRegisteredCompanies(city="Parikkala", result_limit=50)
